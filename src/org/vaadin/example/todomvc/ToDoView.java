420fa30e6b0475a489e27514a9419f6d9f5fff3f
Jouni Koivuviita <jouni@jouni.me>
2014-04-23 13:47:38 +0300

    Todo rows added with different states

--- src/org/vaadin/example/todomvc/ToDoView.java
+++ src/org/vaadin/example/todomvc/ToDoView.java
@@ -2,37 +2,14 @@ package org.vaadin.example.todomvc;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Link;
+import com.vaadin.ui.NativeButton;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
 public class ToDoView extends VerticalLayout implements View {
-
-    Link routingAll;
-
-    Link routingActive;
-
-    Link routingCompleted;
-
-    TextField taskText;
-
-    Label remainingTasksCount;
-
-    Label remainingTasksLabel;
-
-    CssLayout mainSection;
-
-    CssLayout todoStatsContainer;
-
-    Label clearTasksCount;
-
-    Button clearCompleted;
-
-    CheckBox toggleAll;
 
     public ToDoView() {
 
@@ -42,38 +19,59 @@ public class ToDoView extends VerticalLayout implements View {
 
         TextField newTodo = new TextField();
         newTodo.addStyleName("new-todo");
+        newTodo.setInputPrompt("What needs to be done?");
         addComponent(newTodo);
 
-    }
+        addComponent(new CssLayout() {
+            {
+                addStyleName("main");
+                CheckBox toggleAll = new CheckBox("Mark all as complete");
+                toggleAll.addStyleName("toggle-all");
+                addComponent(toggleAll);
 
-    public String getTaskText() {
-        return taskText.getValue();
-    }
+                addComponent(getNewTodoRow("This item is done", true, false));
+                addComponent(getNewTodoRow(
+                        "This one is still not done, and has a very long caption which should span a few lines",
+                        false, false));
+                addComponent(getNewTodoRow(
+                        "This one is not done and is being edited", false, true));
+            }
+        });
 
-    public void clearTaskText() {
-        taskText.setValue("");
-    }
-
-    public void setTaskStatistics(int totalTasks, int completedTasks) {
-        int remainingTasks = totalTasks - completedTasks;
-
-        mainSection.setVisible(totalTasks == 0);
-        todoStatsContainer.setVisible(totalTasks == 0);
-        clearCompleted.setVisible(completedTasks == 0);
-
-        remainingTasksCount.setValue(Integer.toString(remainingTasks));
-        remainingTasksLabel
-                .setValue(remainingTasks > 1 || remainingTasks == 0 ? "items"
-                        : "item");
-        clearTasksCount.setValue(Integer.toString(completedTasks));
-
-        toggleAll.setValue(totalTasks == completedTasks);
     }
 
     @Override
     public void enter(ViewChangeEvent event) {
         // TODO Auto-generated method stub
 
+    }
+
+    CssLayout getNewTodoRow(String captionText, boolean done, boolean editing) {
+        CssLayout row = new CssLayout();
+        row.addStyleName("todo-row");
+        if (done) {
+            row.addStyleName("completed");
+        }
+        if (editing) {
+            row.addStyleName("editing");
+        }
+
+        CheckBox toggle = new CheckBox(null, done);
+        row.addComponent(toggle);
+
+        Label caption = new Label(captionText);
+        caption.setSizeUndefined();
+        row.addComponent(caption);
+
+        NativeButton destroy = new NativeButton();
+        destroy.addStyleName("destroy");
+        row.addComponent(destroy);
+
+        TextField edit = new TextField();
+        edit.setValue(captionText);
+        row.addComponent(edit);
+
+        return row;
     }
 
 }
